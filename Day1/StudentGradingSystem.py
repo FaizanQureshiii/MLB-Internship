7790a9d87243a25e00e8fd88976b54c1d4e49133
Muhammad Faizan <muhammadfaizanqureshiii1@gmail.com>
2026-07-10 16:16:38 +0500

    create StudentGradingSystem
    developed a grading system in python on day 1

---
+++ Day1/StudentGradingSystem.py
@@ -0,0 +1,31 @@
+
+#Student Grading System that takes student name, class, subjects, and marks as input, calculates the average, and assigns grades (A, B, C, F)
+StudentName=input("enter student name: ")
+StudentClass=input("enter student class: ")
+Numofsubjects=int(input("enter number of subjects: "))
+totalMarks=0
+for i in range(Numofsubjects):
+    studentSubject=input("enter subject name: ")
+    studentMarks=int(input("enter marks: "))
+    totalMarks+=studentMarks
+
+averageMarks=totalMarks/Numofsubjects
+
+print("\n")
+
+print("Student Name:", StudentName)
+print("Student Class:", StudentClass)
+print("Total Marks:", totalMarks)
+print("Average Marks:", averageMarks)
+
+if averageMarks>=90:
+    print("Grade: A")
+elif averageMarks<90 and averageMarks>=70:
+    print("Grade: B")
+elif averageMarks>=60 and averageMarks<70:
+    print("Grade: C")
+elif averageMarks<60 and averageMarks>=50:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
